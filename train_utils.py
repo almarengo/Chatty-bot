c431684bf5242e8b7456b2e2cdce044864627838
Alberto Marengo <marengo.albert@gmail.com>
2021-08-18 23:47:35 -0700

    Created train_utils to make inputs and epoch train

---
+++ train_utils.py
@@ -0,0 +1,75 @@
+from load_utils import prepare_data
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+from tqdm.notebook import tnrange, tqdm_notebook
+
+def epoch_train(model, optimizer, batch_size, pairs):
+    
+    # Set the model in train mode
+    model.train()
+    
+    # Gets number total number of rows for training
+    n_records = len(pairs)
+    
+    # Shuffle the row indexes 
+    perm = np.random.permutation(n_records)
+    
+    st = 0
+    
+    while st < n_records:
+        
+        ed = st + batch_size if (st + batch_size) < n_records else n_records
+    
+        encoder_in, decoder_in = to_batch_sequence(pairs, st, ed, perm, device)
+
+        # Calculate outputs and loss
+        output_values, loss = model(encoder_in, decoder_in)
+        
+        # Clear gradients (pytorch accumulates gradients by default)
+        optimizer.zero_grad() 
+
+        # Backpropagation & weight adjustment
+        loss.backward()
+        optimizer.step()
+        
+        st = ed
+
+print(f"Optimization ended successfully")
+
+
+
+def to_batch_sequence(pairs, st, ed, perm, device):
+    
+    encoder_in = []
+    decoder_in = []
+    for i in range(st, ed):
+        
+        pair_batch = pairs[perm[i]]
+        encoder_in.append(pair_batch[0])
+        decoder_in.append(pair_batch[1])
+    
+    encoder_in = [[q.word2index.get(idx) for idx in encoder_in[row].split()] for row in range(len(encoder_in))]
+    decoder_in = [[q.word2index.get(idx) for idx in decoder_in[row].split()] for row in range(len(decoder_in))]
+    
+    encoder_lengths = [len(row) for row in encoder_in]
+    decoder_lengths = [len(row) for row in decoder_in]
+    
+    max_encoder_length = max(encoder_lengths)
+    max_decoder_length = max(decoder_lengths)
+    
+    encoder_in_tensor = torch.zeros(ed, max_encoder_length, device=device, dtype=torch.float)
+    decoder_in_tensor = torch.zeros(ed, max_decoder_length, device=device, dtype=torch.float)
+    
+    for i, seq in enumerate(encoder_in):
+        for t, word in enumerate(seq):
+            if type(word) == int:
+                encoder_in_tensor[i, t] = word 
+                
+    for i, seq in enumerate(decoder_in):
+        for t, word in enumerate(seq):
+            if type(word) == int:
+                decoder_in_tensor[i, t] = word 
+        
+    return encoder_in_tensor, decoder_in_tensor
