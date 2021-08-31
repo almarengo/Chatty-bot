fc803c87d6dac1dcd9a1bf4e0c158edc153b1310
Alberto Marengo <marengo.albert@gmail.com>
2021-08-30 20:18:42 -0700

    Train model file before run

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch
 from tqdm.notebook import tnrange, tqdm_notebook
 from train_utils import *
 from pylive import live_plotter
+import datetime
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -25,22 +26,31 @@ for i, word in enumerate(q.word2index):
 
 # Initiate Model
 
+line1 = []
 
-# Now run for 1000 epochs
+# Now run for 100 epochs
 for epoch in tnrange(2, desc="Total epochs: "):
+
+    print(f'Epoch {epoch + 1}: {datetime.datetime.now()}')
   
     # Calculte loss
     loss = epoch_train(model, optimizer, batch_size, pairs, device)
 
+    print(f'Loss: {loss}')
+
     # Calculate accuracy
     accuracy = epoch_accuray(model, batch_size, pairs, a, device)
+
+    print(f'Train accuracy: {accuracy}')
 
     # Calculate BLEU Score
     BLEU_model = CalculateBleu(model, batch_size, pairs, q, a, device)
     bleu_score = BLEU_model.score()
 
+    print(f'BLUE score: {bleu_score}')
+
     # Try to do interactive plot
-    
+    line1 = live_plotter(epoch, loss, line1)
 
     
 
