04e0854c7cb24e21b502179d46e5f58b21772a1f
Alberto Marengo <marengo.albert@gmail.com>
2021-09-16 06:54:14 -0700

    Adjusted embedding layer taking in Glove weights

--- modules.py
+++ modules.py
@@ -13,6 +13,7 @@ class Encoder(nn.Module):
         self.hidden_size = hidden_size
         self.embedding_dim = embedding_dim
         self.embedding = nn.Embedding(vocabolary_size, embedding_dim, device=device)
+        weights_matrix = torch.tensor(weights_matrix, device = device)
         self.embedding.load_state_dict({'weight': weights_matrix})
         self.embedding.weight.requires_grad = False
         self.embedding.to(device)
