68b2dcc8280f210c8c49a9c2188a607e3f18826f
Alberto Marengo <marengo.albert@gmail.com>
2021-09-29 23:10:16 -0700

    Parser and test run

--- train.py
+++ train.py
@@ -9,9 +9,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import datetime
 import argparse
+import time
+import random
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('n_epochs', type=int, default=100,
+            help='If set, number of epochs to train the model')
     parser.add_argument('--toy', action='store_true', 
             help='If set, use small data; used for fast debugging.')
     parser.add_argument('--dot', action='store_true', 
@@ -28,20 +32,33 @@ if __name__ == '__main__':
     N_word=300
     hidden_size = 100
     dropout = 0.2
+
+    if args.n_epochs:
+        n_epochs=args.n_epochs
+        print(f'Epochs training: {args.n_epochs}')
+    else:
+        n_epochs=100
+        print(f'Epochs training: 100')
     
     if args.toy:
         use_small=True
+        print('Using small')
     else:
         use_small=False
+        print('Using big')
 
     if args.dot:
         att = 'dot'
+        print('Using dot attention')
     if args.general:
         att = 'general'
+        print('Using general attention')
     if args.concat:
         att = 'concat'
+        print('Using concat attention')
     else:
         att = 'general'
+        print('Using general attention')
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -62,8 +79,10 @@ if __name__ == '__main__':
     
     if args.sgd:
         optimizer = 'SGD'
+        print('Using SGD Optimizer')
     else:
         optimizer = 'Adam'
+        print('Using Adam Optimizer')
     batch_size = 32
     lr = 0.0005
 
@@ -73,6 +92,31 @@ if __name__ == '__main__':
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     else:
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    
+    xdata = []
+    ydata1 = []
+    ydata2 = []
+    ydata3 = []
+    
+    plt.show()
+    plt.style.use('ggplot') 
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+
+
+    line1, = ax1.plot(xdata, ydata1, color='red')
+    line2, = ax2.plot(xdata, ydata2, color='blue', label='Train Accuracy')
+    line3, = ax2.plot(xdata, ydata3, color='orange', label='Val Accuracy')
+    ax2.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss')
+    ax2.set_ylabel('Accuracy')
+    ax1.set_title('Train Loss')
+    ax2.set_title('Accuracy')
+    fig.legend(loc=4)
+    fig.tight_layout()
+
+    max_plot1 = 0
+    max_plot2 = 0
     
 
     for epoch in tqdm(4000, desc="Total epochs: "):
@@ -95,11 +139,34 @@ if __name__ == '__main__':
             BLEU_model = CalculateBleu(model, batch_size, train_pairs, q, a, device)
             bleu_score = BLEU_model.score()
             print(f'BLUE score: {bleu_score}')
-        
-        # Plot
 
-        # Save model
+            # Save model
+            torch.save(model.state_dict(), f'saved_model/seq2seq_{epoch}_{att}')
 
+        # Plot model
+        if loss > max_plot1:
+            max_plot1 = np.ceil(loss)
+        if train_accuracy > max_plot2:
+            max_plot2 = np.ceil(train_accuracy)
+        if val_accuracy > max_plot2:
+            max_plot2 = np.ceil(val_accuracy)
+    
+        xdata.append(epoch)
+        ydata1.append(loss)
+        ydata2.append(train_accuracy)
+        ydata3.append(val_accuracy)
+        line1.set_xdata(xdata)
+        line1.set_ydata(ydata1)
+        line2.set_xdata(xdata)
+        line2.set_ydata(ydata2)
+        line3.set_xdata(xdata)
+        line3.set_ydata(ydata3)
+        ax1.set_xlim(0, epoch)
+        ax1.set_ylim(0, max_plot1)
+        ax2.set_xlim(0, epoch)
+        ax2.set_ylim(0, max_plot2)
+        plt.draw()
+       
     print(f"Optimization ended successfully")
     
 
