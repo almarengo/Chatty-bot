d312d29d63c7e79d267c1b9cc011f8cbecf723b3
Alberto Marengo <marengo.albert@gmail.com>
2021-11-16 22:07:05 -0800

    DDP trial

---
+++ DDP/main.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import torch
+import torch.multiprocessing as mp
+from train import train
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+            help='If set, number of epochs to train the model. Default=100')
+    parser.add_argument('--batch_size', type=int, default=32,
+            help='If set, batch size to train the model. Default=32')
+    parser.add_argument('--pre_trained', action='store_true', 
+            help='If set, load pre-trained model.')
+    parser.add_argument('--toy', action='store_true', 
+            help='If set, use small data; used for fast debugging.')
+    parser.add_argument('--dot', action='store_true', 
+            help='If set, apply dot attention.')
+    parser.add_argument('--general', action='store_true', 
+            help='If set, apply general attention.')
+    parser.add_argument('--concat', action='store_true', 
+            help='If set, apply concatenation attention.')
+    parser.add_argument('--sgd', action='store_true', 
+            help='If set, apply SGD optimizer.')
+    args = parser.parse_args()
+
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(f'On this machine you have {device}') 
+
+    
+    print('__pyTorch VERSION:', torch.__version__)
+    print('__CUDA VERSION')
+    print('__CUDNN VERSION:', torch.backends.cudnn.version())
+    print('__Number CUDA Devices:', torch.cuda.device_count())
+    print('__Devices')
+    
+    if torch.cuda.is_available():
+        print('Active CUDA Device: GPU', torch.cuda.current_device())
+
+    args.world_size = args.gpus * args.nodes                
+    os.environ['MASTER_ADDR'] = '10.57.23.164'              
+    os.environ['MASTER_PORT'] = '8888'
+
+    if torch.cuda.is_available():                      
+        mp.spawn(train, nprocs=args.gpus, args=(args,))
+    else:
+        print('Need to build a train for CPU')
+
+
+if __name__ == '__main__':
+    main()
--- model/seq2seq_model.py
+++ model/seq2seq_model.py
@@ -21,9 +21,9 @@ class Seq2Seq(nn.Module):
         self.SOS_token = 1
         self.EOS_token = 2
         self.PAD_token = 0
-        self.decoder_input = torch.tensor([batch_size*[self.SOS_token]], device=device)
-        self.decoder_outputs = torch.zeros((1, 1, 1), device=device)
-        self.prediction = torch.zeros((1, 1, 1), dtype=torch.long, device=device)
+        #self.decoder_input = torch.tensor([batch_size*[self.SOS_token]], device=device)
+        #self.decoder_outputs = torch.zeros((1, 1, 1), device=device)
+        #self.prediction = torch.zeros((1, 1, 1), dtype=torch.long, device=device)
 
     
     def forward(self, src, trg, enc_length, seq_length, teacher_forcing_ratio = 0.8):
@@ -34,15 +34,15 @@ class Seq2Seq(nn.Module):
 
         #encoder_hidden = self.encoder.initHidden()
         
-        #decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size), device=self.device)
+        decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size), device=self.device)
         #decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size)).to(src.device)
-        decoder_outputs = self.decoder_outputs.new_tensor(np.zeros((batch_size, dec_len, self.output_size)))
+        #decoder_outputs = self.decoder_outputs.new_tensor(np.zeros((batch_size, dec_len, self.output_size)))
         
         encoder_outputs, encoder_hidden = self.encoder(src, enc_length)
         
-        #decoder_input = torch.tensor([batch_size*[self.SOS_token]], device = self.device)
+        decoder_input = torch.tensor([batch_size*[self.SOS_token]], device = self.device)
         #decoder_input = torch.tensor([batch_size*[self.SOS_token]]).to(src.device)
-        decoder_input = self.decoder_input
+        #decoder_input = self.decoder_input
         decoder_hidden = encoder_hidden
         
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
@@ -121,15 +121,15 @@ class Seq2Seq(nn.Module):
         enc_len = enc_length
         
         if not is_pred:
-            #decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size), device=self.device)
+            decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size), device=self.device)
             #decoder_outputs = torch.zeros((batch_size, dec_len, self.output_size)).to(src.device)
-            decoder_outputs = self.decoder_outputs.new_tensor(np.zeros((batch_size, dec_len, self.output_size)))
+            #decoder_outputs = self.decoder_outputs.new_tensor(np.zeros((batch_size, dec_len, self.output_size)))
         
         encoder_outputs, encoder_hidden = self.encoder(src, enc_len)
         
-        #decoder_input = torch.tensor([batch_size*[self.SOS_token]], device = self.device)
+        decoder_input = torch.tensor([batch_size*[self.SOS_token]], device = self.device)
         #decoder_input = torch.tensor([batch_size*[self.SOS_token]]).to(src.device)
-        decoder_input = self.decoder_input
+        #decoder_input = self.decoder_input
         decoder_hidden = encoder_hidden
 
 
@@ -154,9 +154,9 @@ class Seq2Seq(nn.Module):
             
 
         else:
-            #prediction = torch.zeros((batch_size, dec_len), dtype=torch.long, device=self.device)
+            prediction = torch.zeros((batch_size, dec_len), dtype=torch.long, device=self.device)
             #prediction = torch.zeros((batch_size, dec_len), dtype=torch.long).to(src.device)
-            prediction = self.prediction.new_tensor(np.zeros((batch_size, dec_len)))
+            #prediction = self.prediction.new_tensor(np.zeros((batch_size, dec_len)))
             for inp in range(dec_len):
                 decoder_output, decoder_hidden, _ = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
                 # Assigns max prob to position 1 (EOS) to words at end of sequence 
