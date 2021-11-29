94575661f846d3ac5c0eeb6da2048b95c059bba3
Alberto Marengo <marengo.albert@gmail.com>
2021-11-29 07:33:51 -0800

    Initial frontend chatbot js

--- app/app.py
+++ app/app.py
@@ -1,16 +1,15 @@
-from typing import ParamSpecKwargs
 from flask import Flask, render_template, request, jsonify
 
 from chat import get_response
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/')
 def index_get():
     return render_template('base.html')
 
 
-@app.post('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
     text = request.get_json().get('message')
     response = get_response(text)
